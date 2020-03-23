b193c159b2a53f179d63c50f6f979aeb5a0dcf19
Sergio Turrent <seturren@microsoft.com>
2020-03-23 15:29:20 -0600

    Added logic to check for az binary before execution

--- deallocate_vms_vmss.sh
+++ deallocate_vms_vmss.sh
@@ -1,35 +1,41 @@
 #!/bin/bash
 
-### Deallocate ALL VMs  and VMSS in the subscription
+### Deallocate all VMs  and VMSS in my subscription
+
+# check az cli is installed
+if ! $(command -v az >/dev/null 2>&1); then
+    echo -e "Error: missing az command..."
+    exit 1
+fi
 
 echo -e "\nStopping all VMs and VMSS...."
-VM_LIST="$(az vm list | grep -v '^Name\|^---' | awk '{print $1"#"$2}')"
-VMSS_LIST="$(az vmss list | grep -v '^Name\|^---' | awk '{print $1"#"$2}')"
+VM_LIST="$(az vm list -o table | grep -v '^Name\|^---' | awk '{print $1"#"$2}')"
+VMSS_LIST="$(az vmss list -o table | grep -v '^Name\|^---' | awk '{print $1"#"$2}')"
 
 if [ "$VM_LIST" == '' ]; then
-	echo -e "\nNo VMs found...\n"
+    echo -e "\nNo VMs found...\n"
 else
-	echo -e "\nDeallocating all VMs:"
-	for INSTANCE in $(echo "$VM_LIST")
-		do 
-			VM_NAME="$(echo -n "$INSTANCE" | cut -d '#' -f1)"
-			RESOURCE_GROUP="$(echo -n "$INSTANCE" | cut -d '#' -f2)"
-			echo -e "Stopping $VM_NAME from $RESOURCE_GROUP"
-			az vm deallocate -n $VM_NAME -g $RESOURCE_GROUP --no-wait
-	done
+    echo -e "\nDeallocating all VMs:"
+    for INSTANCE in $(echo "$VM_LIST")
+    do
+        VM_NAME="$(echo -n "$INSTANCE" | cut -d '#' -f1)"
+        RESOURCE_GROUP="$(echo -n "$INSTANCE" | cut -d '#' -f2)"
+        echo -e "Stopping $VM_NAME from $RESOURCE_GROUP"
+        az vm deallocate -n $VM_NAME -g $RESOURCE_GROUP --no-wait -o table
+    done
 fi
 
 if [ "$VM_LIST" == '' ]; then
-	echo -e "\nNo VMSS found...\n"
+    echo -e "\nNo VMSS found...\n"
 else
-	echo -e "\nDeallocateting all VMSS intances:"
-	for INSTANCE in $(echo "$VMSS_LIST")
-		do
-			VMSS_NAME="$(echo -n "$INSTANCE" | cut -d '#' -f1)"
-			RESOURCE_GROUP="$(echo -n "$INSTANCE" | cut -d '#' -f2)"
-			echo -e "Stoping $VMSS_NAME from $RESOURCE_GROUP"
-			az vmss deallocate -n $VMSS_NAME -g $RESOURCE_GROUP --instance-ids "*" --no-wait
-	done
+    echo -e "\nDeallocateting all VMSS intances:"
+    for INSTANCE in $(echo "$VMSS_LIST")
+    do
+        VMSS_NAME="$(echo -n "$INSTANCE" | cut -d '#' -f1)"
+        RESOURCE_GROUP="$(echo -n "$INSTANCE" | cut -d '#' -f2)"
+        echo -e "Stoping $VMSS_NAME from $RESOURCE_GROUP"
+        az vmss deallocate -n $VMSS_NAME -g $RESOURCE_GROUP --instance-ids "*" --no-wait -o table
+    done
 fi
 
 echo -e "\nDone...\n"
